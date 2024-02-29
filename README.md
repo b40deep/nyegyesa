6f0e88a08857200c08cb68cd548564f749e893f1
b40deep <boydtwine@gmail.com>
2024-02-29 05:17:51 +0000

    broken updates

---
+++ README.md
@@ -0,0 +1,66 @@
+# Nyegyesa: a speech-based language-learning assistant!
+
+## Scenario
+
+pending update
+
+## Why does this project exist?
+
+I started this project to immerse myself in the world of open-source AI models. My goal is to use this to learn a language by practicing speaking the sentences I actually want to say, rather than the ones on the language-learning app.
+It would entail:
+
+- speech to text model
+- language-to-language translation model
+- text to speech model that will 'speak' the result to me in MY intonation [a voice cloning model] so I can directly copy that.
+
+## How to set it up
+
+### setting up a virtual envirionment
+
+I'm using python Python 3.10.0rc2
+I'm using python venv
+setting up one venv on cmd
+`python -m venv name_of_virtual_env`
+to activate the venv, use `.\name_of_virtual_env\Scripts\Activate.ps1` on PowerShell and `.\name_of_virtual_env\Scripts\activate.bat` on CMD (though this might work for either?)
+if you're using vscode, install the Python extension and set up the venv inside your project folder. This way, VS Code will run the venv automatically when you run a Python file.
+
+### downloading the dependencies
+
+I used a requirements file because it's cleaner for me to keep track of. I added comments to it to let me know which model needs which deps since I'm using more than one model.
+`pip install -r .\requirements.txt`
+I also added a `req_list_from_pip.txt` file that has all the versions, even of dependency dependencies.
+
+### downloading the transformers
+
+```
+git clone https://github.com/huggingface/transformers.git
+cd transformers
+pip install -e .
+```
+
+<!-- ### installing an older torch version to avoid all the problems I ran into
+
+`pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118`
+from https://pytorch.org/get-started/previous-versions/
+
+### symlink to move cache to another location
+
+`mklink /J C:\users\.cache\huggingface E:\boyd_cache\huggingface` -->
+
+### ffmpeg install for the text to speech [currently using bark]
+
+https://www.wikihow.com/Install-FFmpeg-on-Windows to download and install
+then `ffmpeg` and `ffmpeg-python` added to the deps
+
+## tracking the model tests:
+
+-
+
+## snapshot of resource usage:
+
+1. Whisper Speech to Text
+
+- Result: `So, is the Christian Bible in less than 50 words? [was supposed to be 'Summarise the Christian Bible in less than 50 words'`
+- Time taken: `STT finished in 0.0471 minutes`
+
+## thoughts thus far / todo list:
--- whisper_new.py
+++ whisper_new.py
@@ -1,44 +1,30 @@
-# general
-import time
-
-# for whisper stt
-from transformers import pipeline
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from datasets import Audio, load_dataset
 # for gradio
 import gradio as gr
 
 
-#####################################################################
-#################     modularise model imports    ###################
-#####################################################################
 
-llm_tokenizer = None
-llm_model = None
-whisper_model = None
-start_time = None
-bark_processor = None
-voice_preset = None
-
-def loadSTT():
-    print('loading STT works')
-    global whisper_model
-    whisper_model = pipeline("automatic-speech-recognition", model="openai/whisper-base", language='en')
-
-
-
-
-#####################################################################
-#####################     whisper stt      ##########################
-#####################################################################
+# load model and processor
+processor = WhisperProcessor.from_pretrained("openai/whisper-base")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-base")
+forced_decoder_ids = processor.get_decoder_prompt_ids(language="french", task="translate")
 
 def runSTT(audio):
-    print('╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦     runSTT     ╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦')
-    tic = time.perf_counter()
-    result = whisper_model(audio)["text"]
-    toc = time.perf_counter()
-    print(f"################ stt result: { result}")
-    print(f"████████████████ STT finished in {(toc - tic)/60:0.4f} minutes ████████████████")
-    return result
+    # load streaming dataset and read first audio sample
+    # ds = load_dataset("common_voice", "fr", split="test", streaming=True)
+    ds = load_dataset("mozilla-foundation/common_voice_11_0", "en")
+    ds = ds.cast_column("audio", Audio(sampling_rate=16_000))
+    print(f'ds {type(ds)}')
+    input_speech = next(iter(ds))["audio"]
+    print(f'inp {type(input_speech)}')
+    # input_speech = audio
+    input_features = processor(input_speech["array"], sampling_rate=input_speech["sampling_rate"], return_tensors="pt").input_features
 
+    # generate token ids
+    predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
+    # decode token ids to text
+    transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
 
 
 
@@ -51,16 +37,13 @@ def runCombined(mic, text, models):
     # if len(models)>0:
     #     for item in models:
     #         loadSTT() if item == 'STT' else loadLLM() if item == 'LLM' else loadTTS() if item == 'TTS' else None
-    loadSTT()
+    # loadSTT()
     
     to_llm = runSTT(mic)
     # to_tts = runLLM(to_llm)
     # result = runTTS2(to_tts)
     result = to_llm
     return result
-
-
-
 
 #####################################################################
 #####################     gradio UI     #############################
